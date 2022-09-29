359684bd16426fa59c936ccea87149d004558575
Schartelb <106553010+Schartelb@users.noreply.github.com>
2022-09-29 09:01:41 -0400

    full update due to changes
    edit pet validation not occurring on submit.  redirects to same page, so code works, but no validation!

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, flash, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, Pet
 
-from forms import AddPetForm
+from forms import PetForm
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "oh-so-secret"
@@ -17,14 +17,15 @@ connect_db(app)
 @app.route("/")
 def homepage():
     """Show pet's links."""
-    return render_template('home.html')
+    pets = Pet.query.all()
+    return render_template('home.html', pets=pets)
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add_pet():
     """Pet add form; handle adding."""
 
-    form = AddPetForm()
+    form = PetForm()
 
     if form.validate_on_submit():
         name = form.name.data
@@ -33,16 +34,31 @@ def add_pet():
                   photo_url=form.photo_url.data,
                   age=form.age.data,
                   notes=form.notes.data,
-                  available=form.notes.data)
+                  available=form.available.data)
         db.session.add(pet)
         db.session.commit()
-        flash(f"Added {name} to adoption list!")
+        flash(f"Added {name} to adoption list!", "add")
         return redirect("/")
     else:
         return render_template(
             "addpet.html", form=form)
 
 
-@app.route("/<int:pet_id>", methods=["GET"])
-def display_pet(pet_id):
-    return render_template("displaypet.html")
+@app.route("/<int:pet_id>", methods=["GET", "POST"])
+def display_edit_pet(pet_id):
+    pet = Pet.query.get_or_404(pet_id)
+    form = PetForm(obj=pet)
+    import pdb
+    pdb.set_trace()
+    if form.validate_on_submit():
+        pet.name = form.name.data
+        pet.species = form.species.data
+        pet.photo_url = form.photo_url.data
+        pet.age = form.age.data
+        pet.notes = form.notes.data
+        pet.available = form.available.data
+        db.session.commit()
+        flash(f"Updated {pet.name}!", "update")
+        return redirect(f"/{pet.id}")
+    else:
+        return render_template("displaypet.html", pet=pet, form=form)
--- forms.py
+++ forms.py
@@ -1,19 +1,20 @@
 from wsgiref.validate import validator
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, BooleanField
+from wtforms import StringField, IntegerField, BooleanField, SelectField
 from wtforms.validators import InputRequired, Optional
 
+species = [('cat', 'Cat'), ('dog', 'Dog'), ('porcupine', 'Porcupine')]
 
-class AddPetForm(FlaskForm):
+
+class PetForm(FlaskForm):
     """Form for adding/editing pets."""
 
     name = StringField("Name",
                        validators=[InputRequired(message="Name is required")])
-    species = StringField("Species",
+    species = SelectField("Species", choices=species,
                           validators=[InputRequired(message='What species is it?')])
     photo_url = StringField("Photo URL", validators=[Optional()])
     age = IntegerField("Age", validators=[Optional()])
     notes = StringField("Notes", validators=[Optional()])
     available = BooleanField("Available",
-                             default='checked',
-                             validators=[InputRequired(message="Is it adoptable?")])
+                             default='checked')
